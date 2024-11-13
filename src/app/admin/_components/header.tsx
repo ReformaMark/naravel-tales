d4f9363850d819f789b3258643dbb04cf855bb4b
Mark <99254035+ReformaMark@users.noreply.github.com>
2024-11-13 21:03:41 +0800

    chore: build production

--- src/app/admin/_components/header.tsx
+++ src/app/admin/_components/header.tsx
@@ -33,8 +33,6 @@ export default function Header({
                         }
                     </div>
                 ))}
-                    
-                   
                 </BreadcrumbList>
             </Breadcrumb>
         </div>
